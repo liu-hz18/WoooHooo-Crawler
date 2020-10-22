49f7e268394189b3f7c15ff7c9a731911342e84e
yh-liu18 <1076598221@qq.com>
2020-10-22 18:31:00 +0800

    ci bug fix

--- newssave/tests.py
+++ newssave/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class MainTest(TestCase):
+    def setUp(self):
+        pass
+
+    def sample_test(self):
+        self.assertEqual(1, 1)
