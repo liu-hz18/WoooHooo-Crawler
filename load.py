6fae69ab672eeaaf33a2c77b0be144e0ca0e61db
yh-liu18 <1076598221@qq.com>
2020-10-26 09:25:10 +0800

    #2 save data in mangodb on the server

--- load.py
+++ load.py
@@ -1,19 +1,29 @@
 import json
+import pymongo
+
 count=0
 date=[]
 monthday=['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16',
           '17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
 for i in range(0,30):
     date.append(f"202008{monthday[i]}")
-
+totalNews=[]
+myclient = pymongo.MongoClient("mongodb://localhost/")
+mydb = myclient["StaticNews"]
+newsSet=mydb["news"]
 for index in date:
-    dateNews=[]
-    with open(f"newsData/{index}.json", 'r', encoding='utf-8') as load_f:
-        strF = load_f.read()
-        if len(strF) > 0:
-            dateNews = json.loads(strF)
-        else:
-            dateNews = []
-    #print(len(dateNews))
-    count=count+len(dateNews)
+    try:
+        dateNews=[]
+        with open(f"newsData/{index}.json", 'r', encoding='utf-8') as load_f:
+            strF = load_f.read()
+            if len(strF) > 0:
+                dateNews = json.loads(strF)
+            else:
+                dateNews = []
+        count=count+len(dateNews)
+        totalNews=totalNews+dateNews
+        x = newsSet.insert_many(dateNews)
+    except:
+        continue
+
 print(count)
