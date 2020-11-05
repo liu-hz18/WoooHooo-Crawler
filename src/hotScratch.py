37e5b84f6ad516cfd0f3262cc60f27c7ad42e18f
yh-liu18 <1076598221@qq.com>
2020-11-05 16:18:31 +0800

    restruct the codes for hot scratch #25

--- src/hotScratch.py
+++ src/hotScratch.py
@@ -4,6 +4,20 @@ import requests
 import json
 import datetime
 
+def getHotDetail(hot_top):
+    hot_news = []
+    count = 0
+    for news in hot_top:
+        count = count + 1
+        news_dict = {}
+        news_dict['rank'] = count
+        news_dict['title'] = news['title']
+        news_dict['url'] = news['url']
+        news_dict['publish_time'] = news['create_date'] +" "+ news['create_time']
+        hot_news.append(news_dict)
+    return hot_news
+
+
 def getHotClick(todayDate):
     base_url = "http://top.news.sina.com.cn/ws/GetTopDataList.php?top_type=day&top_cat=www_www_all_suda_suda&top_time={}&top_show_num=100&top_order=DESC&js_var=all_1_data01"
     Request = base_url.format(todayDate)
@@ -12,19 +26,7 @@ def getHotClick(todayDate):
     content = response.text
     result = eval(json.dumps(content).replace('var all_1_data01 = ', '').replace(';',''))
     hot_click = json.loads(result)['data']
-    print(hot_click)
-    hot_news = []
-    count = 0
-    for news in hot_click:
-        count = count +1
-        news_dict = {}
-        news_dict['rank'] = count
-        news_dict['title'] = news['title']
-        news_dict['url'] = news['url']
-        news_dict['publish_time'] = news['create_date'] + news['create_time']
-        hot_news.append(news_dict)
-    print(hot_news)
-    print(len(hot_news))
+    return hot_click
 
 def getHotComment(todayDate):
     base_url = "http://top.news.sina.com.cn/ws/GetTopDataList.php?top_type=day&top_cat=qbpdpl&top_time={}&top_show_num=100&top_order=DESC&js_var=comment_all_data"
@@ -34,25 +36,16 @@ def getHotComment(todayDate):
     content = response.text
     result = eval(json.dumps(content).replace('var comment_all_data = ', '').replace(';',''))
     hot_comment = json.loads(result)['data']
-    print(hot_comment)
-    hot_news = []
-    count = 0
-    for news in hot_comment:
-        count = count +1
-        news_dict = {}
-        news_dict['rank'] = count
-        news_dict['title'] = news['title']
-        news_dict['url'] = news['url']
-        news_dict['publish_time'] = news['create_date'] + news['create_time']
-        hot_news.append(news_dict)
-    print(hot_news)
-    print(len(hot_news))
-
-def getHotDetail():
-    pass
+    return hot_comment
 
 if __name__ == "__main__":
     nowDate= str(datetime.datetime.now())
     nowDate = nowDate.replace('-','')[:8]
-    getHotClick(nowDate)
-    getHotComment(nowDate)
+    hot_click = getHotClick(nowDate)
+    hot_comment = getHotComment(nowDate)
+    hot_click_news = getHotDetail(hot_click)[0:10]
+    hot_comment_news = getHotDetail(hot_comment)[0:10]
+    print(hot_click_news)
+    print(len(hot_click_news))
+    print(hot_comment_news)
+    print(len(hot_comment_news))
